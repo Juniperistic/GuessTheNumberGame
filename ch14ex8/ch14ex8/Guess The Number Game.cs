77ffada7194cb8a9b6e2ab435db0252aa3669738
Miranda Morris <94880693+Juniperistic@users.noreply.github.com>
2023-07-15 19:37:48 -0400

    Upload of Guess The Number Game

---
+++ ch14ex8/ch14ex8/Guess The Number Game.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch14ex8
+{
+    public partial class Form1 : Form
+    {
+        int guessedNumber = 0;
+        static int computerNumber = 0;
+        static string msg;
+        static int difference = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            guessedNumber = Convert.ToInt32(textBox1.Text);
+
+            if (Math.Abs (computerNumber - guessedNumber) < difference)
+                textBox1.BackColor = Color.Red;
+            else 
+                textBox1.BackColor = Color.Blue;
+
+            if (guessedNumber > computerNumber)
+            {
+                msg = "too high";
+                textBox1.Focus();
+            }
+            else if (guessedNumber < computerNumber)
+            {
+                msg = "too low";
+                textBox1.Focus();
+            }
+            else
+            {
+                msg = "correct!";
+                textBox1.BackColor = Color.Green;
+                button1.Enabled = false;
+            }
+
+            label2.Text = msg;
+
+            difference = Math.Abs (computerNumber - guessedNumber);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void init()
+        {
+            var rand = new Random();
+            computerNumber = rand.Next(1, 1001);
+            computerNumber = 800; //testing
+            button1.Enabled = true;
+            textBox1.Clear();
+            textBox1.BackColor = Color.White;
+            label2.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ch14ex8/ch14ex8/ch14ex8.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*************************************************************************/
+/* Program Name:     Ch14Ex8.cs                                          */
+/* Date:             03/22/2022                                     	 */
+/* Programmer:       Miranda Morris                                 	 */
+/* Class:            CSCI 234                                            */
+/*                                                    				     */
+/* Program Description: The purpose of this program is to create a number*/
+/* guessing game that generates a random number that must be guessed.    */
+/* Additionally, the colors of the textbox are meant to change based on  */
+/* how close or far the number you guessed is from the generated number. */
+/*                                                                       */
+/* Input: a number that you are guessing equals the generated one        */
+/*                                                                       */
+/* Output: whether the number is less than, greater than, or equal to    */
+/* the generated number                                                  */
+/*                                                                       */
+/* Givens: The number is between 1-1000                             	 */
+/*                                                                       */
+/* Testing Data:                                                         */
+/*                                                                       */
+/* List the Testing Input Data: computerNumber = 800;                    */
+/*                                                                       */
+/* List the Testing Output Data:  400, blue, too low                     */
+/*                                450, red, too low                      */
+/*                                850, red, too high                     */
+/*                                800, green, correct, also enter a new  */
+/*                                guess button is disabled now           */
+/*                                                                       */
+/*************************************************************************/
+
+namespace ch14ex8
+{
+    static class ch14ex8
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
